38ab9d49830391636e73946824d350f6436130d6
Marco <ma.souza.junior@gmail.com>
2021-05-26 20:06:16 -0300

    fix asset prefix

--- next.config.js
+++
@@ -1,7 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-const basePath = `/${process.env.BASE_PATH}`;
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? basePath : '',
-};
