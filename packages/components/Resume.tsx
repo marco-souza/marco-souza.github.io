84e169e785ec421a9406d1ae2dae4c414443ac2e
Marco <ma.souza.junior@gmail.com>
2021-07-02 14:33:31 -0300

    fea: add relevant experiences to resume

--- packages/components/Resume.tsx
+++ packages/components/Resume.tsx
@@ -8,20 +8,27 @@ import {
   Image,
   SkeletonCircle,
   SkeletonText,
+  BoxProps,
+  useColorModeValue,
+  Link,
 } from "@chakra-ui/react";
 import { useGithubProfile } from "@packages/features/github-profile";
 import resume from "@packages/config/resume.yml";
-import ReactMarkdown from "react-markdown";
+import ReactMarkdown, { ReactNode } from "react-markdown";
+import { LinkedInIcon } from "@packages/icons";
 
 function Resume() {
   return (
     <Grid templateColumns="repeat(3, 1fr)" gap={4}>
-      <GridItem colSpan={{ md: 1, sm: 3 }} shadow="2xl">
+      <GridItem colSpan={{ md: 1, sm: 3 }}>
         <Sidebar />
       </GridItem>
 
       <GridItem colSpan={{ md: 2, sm: 3 }}>
-        <CoverLetter />
+        <Grid gap="1rem">
+          <CoverLetter />
+          <Experiences />
+        </Grid>
       </GridItem>
     </Grid>
   );
@@ -30,7 +37,7 @@ function Resume() {
 function Sidebar() {
   const { name, avatar, description } = useGithubProfile();
   return (
-    <>
+    <Box shadow="2xl">
       <Box height="18rem">
         <Box backgroundColor="blue.300" height="60%" p={10}></Box>
 
@@ -66,29 +73,81 @@ function Sidebar() {
 
       {/* // TODO: add "download resume" button */}
       <Box backgroundColor="tomato" p="1" />
-    </>
+    </Box>
   );
 }
 
 function CoverLetter() {
   return (
-    <>
-      <Box shadow="dark-lg" p={2} fontSize="1rem">
+    <CardLayout backgroundColor="purple" title="Cover Letter">
+      {resume.cover_letter.split("\n").map((text: string) => (
+        <Box key={text} margin="0.5rem 0" fontSize="0.9rem">
+          <ReactMarkdown>{text}</ReactMarkdown>
+        </Box>
+      ))}
+    </CardLayout>
+  );
+}
+
+function Experiences() {
+  const employDateColor = useColorModeValue("gray.500", "gray.200");
+  return (
+    <CardLayout backgroundColor="green.300" title="Relevant Experiences">
+      {resume.relevant_experiences.map((experience) => (
+        <Box key={experience.company} m="2rem 0 1rem" fontSize="0.9rem">
+          <Heading as="h4" fontSize="1rem">
+            {experience.company}
+          </Heading>
+          <Text color={employDateColor} fontSize="0.8rem" mb="0.5rem">
+            {experience.dates_employed}
+            {/* TODO: calculate time worked using date-fns */}
+          </Text>
+          <Text>{experience.description}</Text>
+          <Box>
+            {experience.tech_stack.map((tech) => (
+              <Text
+                key={tech}
+                as="span"
+                color="blue.300"
+                marginRight="0.5rem"
+                _hover={{
+                  color: "teal.500",
+                  cursor: "pointer",
+                }}
+              >
+                {`#${tech} `}
+              </Text>
+            ))}
+          </Box>
+        </Box>
+      ))}
+
+      <Box m="2rem 0 1rem" textAlign="center" fontSize="0.8rem">
+        <Link border="1px solid" p="5px 10px" borderRadius="md" color="blue.300" href={resume.social.linkedin} target="_blank">
+          See more experiences <LinkedInIcon />
+        </Link>
+      </Box>
+    </CardLayout>
+  );
+}
+
+interface CardLayoutProps extends Pick<BoxProps, "backgroundColor"> {
+  readonly title: string;
+  readonly children: ReactNode;
+}
+
+function CardLayout({ children, backgroundColor, title }: CardLayoutProps) {
+  return (
+    <Box shadow="2xl">
+      <Box p={2} fontSize="1rem">
         <Heading as="h3" size="md" margin="1rem 0">
-          Cover Letter
+          {title}
         </Heading>
 
-        <Box>
-          {resume.cover_letter.split('\n').map((text: string) => (
-            <Box key={text} margin="0.5rem 0" fontSize="0.9rem">
-              <ReactMarkdown>{text}</ReactMarkdown>
-            </Box>
-          ))}
-        </Box>
+        <Box>{children}</Box>
       </Box>
-
-      <Box backgroundColor="purple" p="1" />
-    </>
+      <Box backgroundColor={backgroundColor} p="1" />
+    </Box>
   );
 }
 
