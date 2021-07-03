1f9f1e5f039f74d95571c74a1478c4e5fbfdb395
marco-souza <ma.souza.junior@gmail.com>
2021-07-03 18:14:59 +0000

    Deploying to gh-pages from  @ d86d494f29e8d55f197c90bfe91c11778702de8a 🚀

---
+++ _next/static/OCcWrxICuiZpwo1h6lL5L/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/534-d257657d7f12a1f7471c.js","static/chunks/pages/index-f43b226a5c142bd7d977.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
