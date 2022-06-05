4eeba00e6cee97af91e312a8a0343fbbc63dd12b
Marco Antônio <ma.souza.junior@gmail.com>
2022-06-05 14:11:54 -0300

    fix: formatter issue

--- src/shared/formatters.ts
+++ src/shared/formatters.ts
@@ -1,10 +1,12 @@
 import { config } from "virtual:config";
 
-export function parseBioText(text: string) {
+export function parseBioText(text: string | undefined): string {
+  if (text == null) return "";
+
   const tagRegex = /\@(\w*)/g;
   const linksMap: Map<string, string> = new Map(Object.entries(config.links));
 
-  return text.replace(tagRegex, (originalText, name: string) => {
+  return text?.replace(tagRegex, (originalText, name: string) => {
     const link = linksMap.get(name);
 
     if (link)
