44a98b30d47fa4722d30c46d8ed4747e9bd88c83
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-07 13:53:46 -0300

    enh: adjust home spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Component } from "solid-js";
 
 export const Footer: Component = () => {
   return (
-    <footer class="py-16">
+    <footer class="py-10">
       <p class="text-center text-white text-sm">Made with ☕️, 💻, and ❤️</p>
     </footer>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,21 +9,23 @@ const { site } = config;
 
 export const Home: Component = () => {
   return (
-    <div class="grid grid-cols-1 items-center text-center text-gray-200">
-      <img
-        alt="It's Me"
-        src={profile.avatar_url}
-        class="my-10 h-40 mx-auto rounded-full"
-      />
+    <div class="grid gap-10 my-8 xl:my-16 2xl:my-32 items-center text-center text-gray-200">
+      <div class="gap-2 flex flex-col">
+        <img
+          alt="It's Me"
+          src={profile.avatar_url}
+          class="h-40 mx-auto rounded-full"
+        />
 
-      <div class="text-3xl font-light">{profile.name}</div>
+        <div class="text-3xl font-light">{profile.name}</div>
+      </div>
 
       <div
-        class="opacity-70 font-extralight m-1 text-md text-gray-400 sm:px-20 md:px-40"
+        class="opacity-70 font-extralight text-md text-gray-400 sm:px-20 md:px-40"
         innerHTML={parseBioText(profile.bio)}
       />
 
-      <div class="grid gap-8 grid-cols-1 my-10 sm:mx-20 sm:grid-cols-2">
+      <div class="grid gap-8 grid-cols-1 sm:mx-20 sm:grid-cols-2">
         <ButtonLink
           to={site.cta.primary.link}
           class={`text-pink-400 ${buttonAnimation}`}
