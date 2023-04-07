d9d5ccc750d3ef3a478fad0ad09861d4537e84fd
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-07 11:14:28 -0300

    fea: setup solid-start
    - config to generate static assets
    - update CI

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,8 @@
-import { defineConfig, Plugin } from "vite";
-import solidPlugin from "vite-plugin-solid";
-import Unocss from "unocss/vite";
-import { presetUno, presetWebFonts } from "unocss";
-import tsconfigPaths from "vite-tsconfig-paths";
+import solid from "solid-start/vite";
+import solidStartStatic from "solid-start-static";
+import { defineConfig } from "vite";
 
-import { githubPlugin } from "./plugins/github-plugin";
-import { configPlugin } from "./plugins/yaml-config-plugin";
-import { postsPlugin } from "./plugins/posts-plugin";
-
-const times = (length: number) => Array.from({ length }).map((_, i) => i + 1);
-
-const safePrefix = (prefix: string, size = 10) =>
-  times(size).map((i) => `${prefix}${i}`);
-
-const unoCssSetup = Unocss({
-  safelist: [...safePrefix("grid-cols-")],
-  presets: [
-    presetUno(),
-    presetWebFonts({
-      provider: "google",
-      fonts: {
-        sans: "Roboto",
-        serif: "Times New Roman",
-      },
-    }),
-  ],
+export default defineConfig({
+  base: "/", // base URL for the projects
+  plugins: [solid({ adapter: solidStartStatic() })],
 });
-
-const config = defineConfig({
-  plugins: [
-    solidPlugin(),
-    configPlugin({ relativePath: "./src/settings.yml" }),
-    githubPlugin({ username: "marco-souza" }),
-    postsPlugin(),
-    tsconfigPaths(),
-    unoCssSetup,
-  ],
-  build: {
-    target: "esnext",
-  },
-});
-
-export default config;
