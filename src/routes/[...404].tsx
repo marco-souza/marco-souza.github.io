69a45d29a0a85b5d79ace1085ce6d66f47e63622
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-07 13:54:31 -0300

    fea: add 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,31 +1,26 @@
-import { A } from "solid-start";
+import type { Component } from "solid-js";
+import { Title } from "solid-start";
+import { config } from "virtual:config";
+import { ButtonLink } from "~/components/shared";
 
-export default function NotFound() {
+const { site } = config;
+
+export const Page404: Component = () => {
   return (
-    <main class="text-center mx-auto text-gray-700 p-4">
-      <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Not Found
-      </h1>
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>
-      </p>
-    </main>
+    <>
+      <Title>{site.title} - Not found</Title>
+
+      <div class="text-center my-[15vh] text-gray-100 items-center justify-center flex flex-col gap-4">
+        <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
+        <h2 class="bg-pink-500 px-2 text-sm rounded rotate-12 absolute">
+          Page Not Found
+        </h2>
+        <ButtonLink to="/" class="border-pink-500 text-pink-500">
+          Go Home
+        </ButtonLink>
+      </div>
+    </>
   );
-}
+};
+
+export default Page404;
