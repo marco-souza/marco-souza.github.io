63e0bccd3df0982d16774f961e4ee267a513ac61
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-07 17:52:23 -0300

    enh: share NotFound component

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,6 +1,7 @@
 import type { Component } from "solid-js";
 import { Title } from "solid-start";
 import { config } from "virtual:config";
+import NotFound from "~/components/NotFound";
 import { ButtonLink } from "~/components/shared";
 
 const { site } = config;
@@ -10,15 +11,11 @@ export const Page404: Component = () => {
     <>
       <Title>{site.title} - Not found</Title>
 
-      <div class="text-center my-[15vh] text-gray-100 items-center justify-center flex flex-col gap-4">
-        <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
-        <h2 class="bg-pink-500 px-2 text-sm rounded rotate-12 absolute">
-          Page Not Found
-        </h2>
+      <NotFound>
         <ButtonLink to="/" class="border-pink-500 text-pink-500">
           Go Home
         </ButtonLink>
-      </div>
+      </NotFound>
     </>
   );
 };
