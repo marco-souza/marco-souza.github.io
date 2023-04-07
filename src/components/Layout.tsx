14b13dcc211fdd1866b4cbfe15423b73410cbf1a
Marco Antônio <ma.souza.junior@gmail.com>
2023-04-07 13:48:13 -0300

    enh: move meta data to root.tsx layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,23 +1,15 @@
 import { Component } from "solid-js";
-import { Meta, Title } from "solid-start";
-import { config } from "virtual:config";
 
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
 import { ChildrenProps, Container } from "./shared";
 
-const { site } = config;
-
 export const Layout: Component<ChildrenProps> = (props) => {
   return (
-    <div class="min-h-screen bg-gray-700">
-      {/* Meta tags */}
-      <Title>{site.title}</Title>
-      <Meta name="description" content={site.description} />
-
+    <main class="min-h-screen bg-gray-700 relative">
       <Navbar />
-      <Container class="pt-24 min-h-80vh">{props.children}</Container>
+      <Container class="pt-24 min-h-[80vh]">{props.children}</Container>
       <Footer />
-    </div>
+    </main>
   );
 };
--- src/root.tsx
+++ src/root.tsx
@@ -11,14 +11,20 @@ import {
   Scripts,
   Title,
 } from "solid-start";
+import { config } from "virtual:config";
 import { Layout } from "~/components/Layout";
 import "~/root.css";
+
+const { site } = config;
 
 export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With TailwindCSS</Title>
+        {/* Meta tags */}
+        <Title> {site.title}</Title>
+        <Meta name="description" content={site.description} />
+
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
