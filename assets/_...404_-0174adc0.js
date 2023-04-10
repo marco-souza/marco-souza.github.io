88cfabce15e9ba30eab23e5587e271b625373402
marco-souza <ma.souza.junior@gmail.com>
2023-04-10 23:48:42 +0000

    Deploying to gh-pages from  @ 5f19b999acd194df774f482ab87f095f2e831fe9 🚀

---
+++ assets/_...404_-0174adc0.js
@@ -0,0 +1,1 @@
+import{c as t,a as e,T as o,B as r,b as n}from"./index-876ff42e.js";import{N as a}from"./NotFound-c4392149.js";const{site:i}=n,d=()=>[t(o,{get children(){return[e(()=>i.title)," - Not found"]}}),t(a,{get children(){return t(r,{to:"/",class:"border-pink-500 text-pink-500",children:"Go Home"})}})];export{d as Page404,d as default};
